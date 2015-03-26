760273140c0479ec342ae5d82a2f01c124657b25
danielgaleano <ramgalb@gmail.com>
2015-03-26 01:38:32 -0400

    Corregido urls usuarios

--- gpsk/apps/usuarios/urls.py
+++ gpsk/apps/usuarios/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('apps',
                        url(r'^$', login_required(views.UserIndexView.as_view()), name='index'),
                        url(r'^create/$', login_required(views.UserCreate.as_view()), name='create'),
                        url(r'^update/(?P<pk>\d+)/$', login_required(views.UserUpdate.as_view()), name='update'),
